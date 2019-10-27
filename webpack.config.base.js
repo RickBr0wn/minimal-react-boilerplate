cc4047f7a1220c3849094e417afe222a71d461cb
Rick Brown <ricky.brown.00@gmail.com>
2019-10-27 13:39:40 +0000

    Added corejs@3.3.4

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -24,6 +24,7 @@ module.exports = {
                   'not < 2%',
                   'not ie 11'
                 ],
+                corejs: '3.3.4',
                 useBuiltIns: 'entry'
               }
             ],
