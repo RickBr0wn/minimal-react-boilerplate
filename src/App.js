9598ecece7c5c7ec2385cf43a3ddf5cafc96ba56
Rick Brown <ricky.brown.00@gmail.com>
2019-10-27 11:09:32 +0000

    Added react-hot-loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { hot } from 'react-hot-loader'
 
 class App extends React.Component {
   state = {
@@ -6,12 +7,13 @@ class App extends React.Component {
   }
 
   render() {
+    const { count } = this.state
     return (
       <div>
         <h1>Development Server</h1>
         <h3>Port: 9000</h3>
         <br />
-        <div>Count: {this.state.count}</div>
+        <div className={count > 5 ? 'warning' : null}>Count: {count}</div>
         <button
           onClick={() =>
             this.setState(prevState => ({ count: prevState.count + 1 }))
@@ -29,4 +31,4 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default hot(module)(App)
