8ed9d8079ef98611c74b515806ab0ac305ab6107
Rick Brown <ricky.brown.00@gmail.com>
2019-10-27 14:46:54 +0000

    Added jest.setup files

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+
 describe('<App />', () => {
-  it('should run and passes a test', () => {
-    expect(true).toBe(true)
+  it('Renders without error', () => {
+    render(<App />)
   })
 })
