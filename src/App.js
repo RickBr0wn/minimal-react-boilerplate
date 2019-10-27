cfe50f87366cfba451dda5fab5d7f4e08a3fdc2e
Rick Brown <ricky.brown.00@gmail.com>
2019-10-27 20:19:04 +0000

    Cleaned code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-const Warning = React.lazy(() => import('./Warning'))
-
 class App extends React.Component {
-  state = {
-    count: 0
-  }
-
   render() {
-    const { count } = this.state
     return (
       <div>
-        <h1>Development Server</h1>
-        <h2>Port: 9000</h2>
-        <br />
-        <div className={count > 8 ? 'warning' : null}>Count: {count}</div>
-        <button
-          onClick={() =>
-            this.setState(prevState => ({ count: prevState.count + 1 }))
-          }
-        >
-          +
-        </button>
-        <button
-          onClick={() =>
-            this.setState(prevState => ({ count: prevState.count - 1 }))
-          }
-        >
-          -
-        </button>
-        {count > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
+        <h1>Minimal React Boilerplate</h1>
+        <div>Rick Brown &#169; {new Date().getFullYear()}</div>
       </div>
     )
   }
